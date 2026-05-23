f1373a5ab78d4f7de4dec7918d62941d709a7d8b
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2026-05-23 19:18:04 +0900

    feat: 新規作成

---
+++ keyforge-pico.c
@@ -0,0 +1,20 @@
+#include "pico/stdlib.h"
+
+#define LED_PIN 25
+#define BLINK_INTERVAL_MS 500
+
+int main()
+{
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while (true)
+    {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(BLINK_INTERVAL_MS);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(BLINK_INTERVAL_MS);
+    }
+
+    return 0;
+}
